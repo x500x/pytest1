200a14616069996498345d7489f7ebe153fc4669
x500x <109746464+x500x@users.noreply.github.com>
2024-09-14 12:39:33 +0800

    Add files via upload

--- 1.py
+++ 1.py
@@ -230,8 +230,15 @@ def uploader(file_path):
 
 
 def downloader(url, local_filename):
-    opener=urllib.request.build_opener()
-    opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+    opener = urllib.request.build_opener()
+    # 构建请求头列表每次随机选择一个
+    ua_list = ['Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0',
+           'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+           'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36 Edg/103.0.1264.62',
+           'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0',
+           'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.81 Safari/537.36 SE 2.X MetaSr 1.0'
+           ]
+    opener.addheaders = [('User-Agent', random.choice(ua_list))]
     urllib.request.install_opener(opener)
     try:
        urllib.request.urlretrieve(url, local_filename)
@@ -306,7 +313,7 @@ def threadWorker(f,lock):
 #local_filename = 'large_video.mp4'
 #downloader(url, local_filename)
 thread_list=[]
-with open(os.getcwd()+'\\info.txt',"r") as f:
+with open(os.getcwd()+'\\info.txt',"r", encoding='utf-8') as f:
     for i in range(1,3):
         t=threading.Thread(target=threadWorker,args=(f,threading.Lock()))
         thread_list.append(t)
