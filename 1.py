0eb07ff84631e9a418d0f6d6643c4e7bdfe7a7c4
x500x <109746464+x500x@users.noreply.github.com>
2024-09-14 12:57:28 +0800

    Add files via upload

--- 1.py
+++ 1.py
@@ -281,7 +281,7 @@ def threadWorker(f,lock):
                 retry=0
                 while retry<6:
                     retry+=1
-                    rcode=downloader(url,os.getcwd()+'\\'+name)
+                    rcode = downloader(url, os.path.join(os.getcwd(), name))
                     if 0==rcode:
                         break
                     elif 1==rcode:
@@ -289,7 +289,7 @@ def threadWorker(f,lock):
                     else:
                         break   
                 with lock:
-                    file_list.append(os.getcwd()+'\\'+name)
+                    file_list.append(os.path.join(os.getcwd(), name))
                 url=""
                 name=""
                 break
