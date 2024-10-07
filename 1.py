c9d1a8f535574572c8ab78e7287c13426bb596ef
x500x <109746464+x500x@users.noreply.github.com>
2024-10-07 13:10:01 +0800

    更新 1.py

--- 1.py
+++ 1.py
@@ -40,7 +40,7 @@ def preUpload(md5_hash,filename,filesize):
       "driveId": 0,
       "etag": md5_hash.hexdigest(),
       "fileName": str(filename),
-      "parentFileId": 0,
+      "parentFileId": 9549688,
       "size": filesize,
       "type": 0,
       "RequestSource": None,
